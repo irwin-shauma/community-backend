8e2b31b1505cc9e09ec9185d485537794dbe53a9
irwin-shauma <irwin.shauma.rizky@gmail.com>
2022-07-19 10:25:03 +0700

    adding role

--- community/src/main/java/com/lawencon/community/model/Role.java
+++ community/src/main/java/com/lawencon/community/model/Role.java
@@ -1,17 +1,12 @@
 package com.lawencon.community.model;
 
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
-
-import org.hibernate.search.mapper.pojo.mapping.definition.annotation.FullTextField;
-import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
 
 import com.lawencon.base.BaseEntity;
 
 @Entity
-@Indexed
 @Table(uniqueConstraints = {
 		@UniqueConstraint(
 				name = "role_ck",
@@ -21,16 +16,28 @@ import com.lawencon.base.BaseEntity;
 public class Role extends BaseEntity{
 	private static final long serialVersionUID = -5196455701225322056L;
 	
-	@FullTextField
 	private String roleName;
 	
-	@FullTextField
 	private String roleCode;
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+
+	public String getRoleCode() {
+		return roleCode;
+	}
+
+	public void setRoleCode(String roleCode) {
+		this.roleCode = roleCode;
+	}
 	
 	
 	
-	@ManyToOne
-	@Jo
 	
 	
 	
