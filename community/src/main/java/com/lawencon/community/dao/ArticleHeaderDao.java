3da9bcb1f575eee065fc962eaac2d9348ab646ce
Irfan Kurniawan <irfangokil23@gmail.com>
2022-07-20 10:34:29 +0700

    Add New Dao

--- community/src/main/java/com/lawencon/community/dao/ArticleHeaderDao.java
+++ community/src/main/java/com/lawencon/community/dao/ArticleHeaderDao.java
@@ -1,11 +1,49 @@
 package com.lawencon.community.dao;
 
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import com.lawencon.base.AbstractJpaDao;
 import com.lawencon.community.model.ArticleHeader;
+import com.lawencon.community.model.File;
 
 @Repository
 public class ArticleHeaderDao extends AbstractJpaDao<ArticleHeader> {
+	
+	public List<ArticleHeader> findByTitle(String title) throws Exception {
+		String sql = "SELECT * FROM article_header WHERE title = :title";
+		List<?> result = createNativeQuery(sql)
+						.setParameter("title", title)
+						.getResultList();
+		List<ArticleHeader> articles = new ArrayList<ArticleHeader>();
+		
+		result.forEach(obj -> {
+			Object[] objArr = (Object[]) obj;
+			ArticleHeader article = new ArticleHeader();
+			article.setId(objArr[0].toString());
+			article.setArticleHeaderCode(objArr[1].toString());
+			File file = new File();
+			file.setId(objArr[2].toString());
+			article.setFileId(file);
+			article.setTitle(objArr[3].toString());
+			article.setContents(objArr[4].toString());
+			article.setCreatedBy(objArr[5].toString());
+			if (objArr[6] != null) {
+				article.setCreatedAt(((Timestamp) objArr[6]).toLocalDateTime());
+			}
+			article.setUpdatedBy(objArr[7].toString());
+			if (objArr[8] != null) {
+				article.setUpdatedAt(((Timestamp) objArr[8]).toLocalDateTime());
+			}
+			article.setIsActive(Boolean.valueOf(objArr[9].toString()));
+			article.setVersion(Integer.valueOf(objArr[10].toString()));
+			
+			articles.add(article);
+		});
+		return articles;
+	}
 
 }
--- community/src/main/java/com/lawencon/community/dao/BookmarkDao.java
+++ community/src/main/java/com/lawencon/community/dao/BookmarkDao.java
@@ -4,8 +4,42 @@ import org.springframework.stereotype.Repository;
 
 import com.lawencon.base.AbstractJpaDao;
 import com.lawencon.community.model.Bookmark;
+import com.lawencon.community.model.ThreadHeader;
+import com.lawencon.community.model.User;
 
 @Repository
 public class BookmarkDao extends AbstractJpaDao<Bookmark> {
+	
+	public Bookmark findByUserId(String userId) throws Exception {
+		StringBuilder sql = new StringBuilder()
+				.append("SELECT id, user_id, thread_id, is_active, version")
+				.append(" FROM bookmark WHERE user_id = :user_id");
+		Bookmark bookmark = null;
+		
+		try {
+			Object result = createNativeQuery(sql.toString())
+					.setParameter("user_id", userId)
+					.getResultList();
+			if (result != null) {
+				Object[] objArr = (Object[]) result;
+				bookmark = new Bookmark();
+				bookmark.setId(objArr[0].toString());
+				User user = new User();
+				user.setId(objArr[1].toString());
+				bookmark.setUser(user);
+				
+				ThreadHeader thread = new ThreadHeader();
+				thread.setId(objArr[2].toString());
+				bookmark.setThread(thread);
+				
+				bookmark.setIsActive(Boolean.valueOf(objArr[3].toString()));
+				bookmark.setVersion(Integer.valueOf(objArr[4].toString()));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return bookmark;
+	}
 
 }
