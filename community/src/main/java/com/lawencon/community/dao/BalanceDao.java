a715e7d95f97199f35c399555dd8f64696bf82bd
rizqi211199 <rizqialfiyati211199@gmail.com>
2022-07-20 11:18:00 +0700

    add new dao

---
+++ community/src/main/java/com/lawencon/community/dao/BalanceDao.java
@@ -0,0 +1,36 @@
+package com.lawencon.community.dao;
+
+import com.lawencon.base.AbstractJpaDao;
+import com.lawencon.community.model.Balance;
+import com.lawencon.community.model.User;
+
+public class BalanceDao extends AbstractJpaDao<Balance>{
+	public Balance findByUserId(Long userId) throws Exception{
+		StringBuilder sqlBuilder = new StringBuilder();
+		sqlBuilder.append("SELECT b.id, b.balanceCode, b.currentBalance, b.user FROM balance b")
+					.append("INNER JOIN users u ON u.id = b.user")
+					.append("WHERE b.user = :userId");
+		
+		Balance balance =null;
+		try {
+			Object result = createNativeQuery(sqlBuilder.toString())
+					.setParameter("userId", userId)
+					.getSingleResult();
+			
+			if(result != null) {
+				Object[] objArr = (Object[]) result;
+				balance = new Balance();
+				balance.setId(objArr[0].toString());
+				balance.setBalanceCode(objArr[1].toString());
+				balance.setCurrentBalance(Float.valueOf(objArr[2].toString()));
+				
+				User user = new User();
+				user.setId(objArr[3].toString());
+				balance.setUser(user);
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return balance;
+	}
+}
--- community/src/main/java/com/lawencon/community/dao/EventHeaderDao.java
+++ community/src/main/java/com/lawencon/community/dao/EventHeaderDao.java
@@ -4,8 +4,36 @@ import org.springframework.stereotype.Repository;
 
 import com.lawencon.base.AbstractJpaDao;
 import com.lawencon.community.model.EventHeader;
+import com.lawencon.community.model.EventType;
 
 @Repository
 public class EventHeaderDao extends AbstractJpaDao<EventHeader> {
-
+	public EventHeader findByTitle(String title) throws Exception{
+		StringBuilder sqlBuilder = new StringBuilder();
+		sqlBuilder.append("SELECT eh.id, eh.eventHeaderCode, eh.title, eh.eventType FROM event_header eh ")
+					.append(" INNER JOIN event_type et ON et.id = eh.eventType ")
+					.append(" WHERE eh.title = : title ");
+		
+		EventHeader eventHeader = null;
+		try {
+			Object result = createNativeQuery(sqlBuilder.toString())
+					.setParameter("title", title)
+					.getSingleResult();
+			if(result != null) {
+				Object[] objArr = (Object[]) result;
+				eventHeader = new EventHeader();
+				eventHeader.setId(objArr[0].toString());
+				eventHeader.setEventHeaderCode(objArr[1].toString());
+				eventHeader.setTitle(objArr[2].toString());
+				
+				EventType eventType = new EventType();
+				eventType.setId(objArr[3].toString());
+				
+				eventHeader.setEventTypeId(eventType);
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return eventHeader;)
+	}
 }
