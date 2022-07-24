c8366148670df90bd29fc2e69672c1b351408e6f
irwin-shauma <irwin.shauma.rizky@gmail.com>
2022-07-24 10:51:39 +0700

    changing login controller

---
+++ community/src/main/java/com/lawencon/community/config/WebIgnoringConfig.java
@@ -0,0 +1,37 @@
+package com.lawencon.community.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+@Configuration
+public class WebIgnoringConfig {
+
+	private List<RequestMatcher> matchers = new ArrayList<>();
+
+	@Bean
+	@Qualifier("webIgnoring")
+	public List<RequestMatcher> antMatchers() {
+		matchers.add(new AntPathRequestMatcher("/users", HttpMethod.POST.name()));
+		matchers.add(new AntPathRequestMatcher("/swagger-ui.html", HttpMethod.GET.name()));
+		matchers.add(new AntPathRequestMatcher("/students", HttpMethod.POST.name()));
+		
+		
+		matchers.add(new AntPathRequestMatcher("/users/**", HttpMethod.POST.name()));
+		matchers.add(new AntPathRequestMatcher("/users/**", HttpMethod.GET.name()));
+		
+		matchers.add(new AntPathRequestMatcher("/roles/**", HttpMethod.GET.name()));
+		
+		
+		
+		
+		
+		return matchers;
+	}
+}
---
+++ community/src/main/java/com/lawencon/community/dto/user/LoginReq.java
@@ -0,0 +1,20 @@
+package com.lawencon.community.dto.user;
+
+public class LoginReq {
+	private String email;
+	private String password;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+}
