a997e8ffb3664c15a35057bfaa954f4859289898
IrfanKurni <irfangokil23@gmail.com>
2022-08-13 15:35:28 +0700

    event header by user

--- community/src/main/java/com/lawencon/community/dao/EventHeaderDao.java
+++ community/src/main/java/com/lawencon/community/dao/EventHeaderDao.java
@@ -57,31 +57,35 @@ public class EventHeaderDao extends AbstractJpaDao<EventHeader> {
 			eventHeader.setId(objArr[0].toString());
 			eventHeader.setEventHeaderCode(objArr[1].toString());
 			
-			EventType eventType = new EventType();
-			eventType.setId(objArr[2].toString());
-			eventHeader.setEventType(eventType);
-
-			eventHeader.setTitle(objArr[3].toString());
-
-			File file = new File();
-			file.setId(objArr[10].toString());
-
+			EventType eventTypes = new EventType();
+			eventTypes.setId(objArr[2].toString());
+			eventHeader.setEventType(eventTypes);
+			
+			if(objArr[3] != null) {
+				File file = new File();
+				file.setId(objArr[3].toString());
+				eventHeader.setFile(file);
+			}
+			
+			eventHeader.setTitle(objArr[4].toString());
+			
 			User user = new User();
-			user.setId(objArr[11].toString());
-
-			eventHeader.setCreatedAt(((Timestamp)objArr[4]).toLocalDateTime());
-			eventHeader.setCreatedBy(objArr[5].toString());
-
-			if(objArr[6] != null) {
-				eventHeader.setUpdatedAt(((Timestamp)objArr[6]).toLocalDateTime());
+			user.setId(objArr[5].toString());
+			eventHeader.setUser(user);
+			
+			eventHeader.setCreatedAt(((Timestamp)objArr[6]).toLocalDateTime());
+			eventHeader.setCreatedBy(objArr[7].toString());
+			
+			if(objArr[8] != null) {
+				eventHeader.setUpdatedAt(((Timestamp)objArr[8]).toLocalDateTime());
 			}
-
-			if(objArr[7] != null) {
-				eventHeader.setUpdatedBy(objArr[7].toString());
+			
+			if(objArr[9] != null) {
+				eventHeader.setUpdatedBy(objArr[9].toString());
 			}
-
-			eventHeader.setIsActive(Boolean.valueOf(objArr[8].toString()));
-			eventHeader.setVersion(Integer.valueOf(objArr[9].toString()));
+			
+			eventHeader.setIsActive(Boolean.valueOf(objArr[10].toString()));
+			eventHeader.setVersion(Integer.valueOf(objArr[11].toString()));
 
 			eventHeaders.add(eventHeader);
 		});
@@ -117,6 +121,7 @@ public class EventHeaderDao extends AbstractJpaDao<EventHeader> {
 			
 			User user = new User();
 			user.setId(objArr[5].toString());
+			eventHeader.setUser(user);
 			
 			eventHeader.setCreatedAt(((Timestamp)objArr[6]).toLocalDateTime());
 			eventHeader.setCreatedBy(objArr[7].toString());
--- community/src/main/java/com/lawencon/community/dao/EventPaymentHistoryDao.java
+++ community/src/main/java/com/lawencon/community/dao/EventPaymentHistoryDao.java
@@ -108,5 +108,51 @@ public class EventPaymentHistoryDao extends AbstractJpaDao<EventPaymentHistory> 
 		
 	}
 	
+	
+	public List<EventPaymentHistory> findAllByUser(String user,String query, Integer startPage, Integer maxPage) throws Exception {
+		String sql = "SELECT * FROM event_payment_history WHERE user_id = :user";
+		
+		List<?> result = createNativeQuery(sql).setParameter("user", user).getResultList();
+		List<EventPaymentHistory> eventPayments = new ArrayList<>();
+		
+		result.forEach(obj -> {
+			Object[] objArr = (Object[]) obj;
+			EventPaymentHistory eventPayment = new EventPaymentHistory();
+			eventPayment.setId(objArr[0].toString());
+			eventPayment.setEventPaymentCode(objArr[1].toString());
+			
+			Payment payment = new Payment();
+			payment.setId(objArr[2].toString());
+			eventPayment.setPayment(payment);
+			
+			User users = new User();
+			users.setId(objArr[3].toString());
+			eventPayment.setUser(users);
+			
+			EventHeader eventHeader = new EventHeader();
+			eventHeader.setId(objArr[4].toString());
+			eventPayment.setEventHeader(eventHeader);
+			
+			eventPayment.setTrxNo(objArr[5].toString());
+			eventPayment.setCreatedAt(((Timestamp)objArr[6]).toLocalDateTime());
+			eventPayment.setCreatedBy(objArr[7].toString());
+			
+			if(objArr[8] != null) {
+				eventPayment.setUpdatedAt(((Timestamp)objArr[8]).toLocalDateTime());
+			}
+			
+			if(objArr[9] != null) {
+				eventPayment.setUpdatedBy(objArr[9].toString());
+			}
+			eventPayment.setIsActive(Boolean.valueOf(objArr[10].toString()));
+			eventPayment.setVersion(Integer.valueOf(objArr[11].toString()));
+			
+			eventPayments.add(eventPayment);
+		});
+		
+		return eventPayments;
+		
+	}
+	
 
 }
